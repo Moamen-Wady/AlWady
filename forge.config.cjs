000f9fc005f7f2b6c3052680bb44b4d118dd9600
Moamen-Wady <moamen.wady@gmail.com>
2024-07-25 19:12:38 +0300

    first commit

--- forge.config.cjs
+++ forge.config.cjs
@@ -7,14 +7,14 @@ module.exports = {
   },
   rebuildConfig: {},
   makers: [
-    {
-      name: "@electron-forge/maker-squirrel",
-      config: {},
-    },
     // {
-    //   name: "@electron-forge/maker-zip",
-    //   platforms: ["darwin"],
+    //   name: "@electron-forge/maker-squirrel",
+    //   config: {},
     // },
+    {
+      name: "@electron-forge/maker-zip",
+      platforms: ["darwin", "win32"],
+    },
     // {
     //   name: "@electron-forge/maker-deb",
     //   config: {},
